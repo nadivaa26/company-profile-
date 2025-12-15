91fb0012da4c37af65cc5e9f0c60a5fa60f8fc2f
nadivaaa <divaadivaaa49@gmail.com>
2025-12-15 10:09:21 +0700

    Add contact routes for compro controller

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,5 +50,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'compro';
+
+$route['contact'] = 'compro';
+$route['contact/send'] = 'compro/send';
+
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
