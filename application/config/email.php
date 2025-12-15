0be4310398a8e95fcfea5cf50b6e6c47c6df0009
nadivaaa <divaadivaaa49@gmail.com>
2025-12-15 10:08:20 +0700

    Add files via upload

---
+++ application/config/email.php
@@ -0,0 +1,12 @@
+<?php
+$config = [
+    'protocol'  => 'smtp',
+    'smtp_host' => 'smtp.gmail.com',
+    'smtp_user' => 'maliki.edulogi@gmail.com',
+    'smtp_pass' => 'nztjdtakuedswxcb',
+    'smtp_port' => 587,
+    'smtp_crypto' => 'tls',
+    'mailtype'  => 'text',
+    'charset'   => 'utf-8',
+    'newline'   => "\r\n"
+];
