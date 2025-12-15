ee774a146d6888b5956a189429053025731048b2
nadivaaa <divaadivaaa49@gmail.com>
2025-12-15 12:39:42 +0700

    Remove SMTP password from email config
    Removed SMTP password for security reasons.

--- application/config/email.php
+++ application/config/email.php
@@ -3,10 +3,11 @@ $config = [
     'protocol'  => 'smtp',
     'smtp_host' => 'smtp.gmail.com',
     'smtp_user' => 'maliki.edulogi@gmail.com',
-    'smtp_pass' => 'nztjdtakuedswxcb',
+    'smtp_pass' => '',
     'smtp_port' => 587,
     'smtp_crypto' => 'tls',
     'mailtype'  => 'text',
     'charset'   => 'utf-8',
     'newline'   => "\r\n"
 ];
+
