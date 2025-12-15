c899353a749fc053b3de7da43cb84cf0ebc98200
nadivaaa <divaadivaaa49@gmail.com>
2025-12-15 10:07:37 +0700

    Add constructor and send method for form handling

--- application/controllers/Compro.php
+++ application/controllers/Compro.php
@@ -3,23 +3,68 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Compro extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/userguide3/general/urls.html
-	 */
-	public function index()
-	{
-		$this->load->view('index');
-	}
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library(['session', 'email', 'form_validation']);
+        $this->load->helper(['url', 'form']);
+    }
+
+    public function index()
+    {
+        $this->load->view('index');
+    }
+
+    public function send()
+    {
+        // VALIDASI (sesuai name di form)
+        $this->form_validation->set_rules('nama', 'Nama', 'required|min_length[3]');
+        $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+        $this->form_validation->set_rules('telepon', 'No Telepon', 'required|min_length[5]');
+        $this->form_validation->set_rules('pesan', 'Pesan', 'required|min_length[5]');
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->load->view('index');
+            return;
+        }
+
+        $nama  = $this->input->post('nama', TRUE);
+        $email = $this->input->post('email', TRUE);
+        $telepon = $this->input->post('telepon', TRUE);
+        $pesan = $this->input->post('pesan', TRUE);
+
+        // Email ke admin
+  $this->email->from('maliki.edulogi@gmail.com', 'Website Maliki');
+  $this->email->reply_to($email, $nama);
+  $this->email->to('maliki.edulogi@gmail.com');
+   $this->email->subject('Pesan dari Website');
+        $this->email->message("
+        Nama: $nama
+        Telepon: $telepon
+        Email: $email
+
+        Pesan:
+        $pesan
+        ");
+
+        if ($this->email->send()) {
+
+            // Auto reply
+            $this->email->clear();
+            $this->email->from('maliki.edulogi@gmail.com', 'Maliki.ID');
+            $this->email->to($email);
+            $this->email->subject('Pesan Anda Sudah Kami Terima');
+            $this->email->message(
+                'Terima kasih sudah menghubungi kami. Tim kami akan segera merespon.'
+            );
+            $this->email->send();
+
+            $this->session->set_flashdata('success', 'Pesan berhasil dikirim!');
+        } else {
+            $this->session->set_flashdata('error', 'Pesan gagal dikirim!');
+        }
+
+        redirect('compro');
+    }
 }
+
