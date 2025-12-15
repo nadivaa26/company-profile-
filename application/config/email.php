d64e990e86b6d38fdb9b0e02b9a00c79a1594ad5
nadivaaa <divaadivaaa49@gmail.com>
2025-12-15 12:41:15 +0700

    Modify email configuration for SMTP settings
    Updated SMTP configuration with placeholders for user and password.

--- application/config/email.php
+++ application/config/email.php
@@ -2,12 +2,13 @@
 $config = [
     'protocol'  => 'smtp',
     'smtp_host' => 'smtp.gmail.com',
-    'smtp_user' => 'maliki.edulogi@gmail.com',
-    'smtp_pass' => '',
+    'smtp_user' => 'EMAIL',
+    'smtp_pass' => 'APP PASSWORD',
     'smtp_port' => 587,
     'smtp_crypto' => 'tls',
     'mailtype'  => 'text',
     'charset'   => 'utf-8',
     'newline'   => "\r\n"
 ];
+
 
